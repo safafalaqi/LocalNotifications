18ade7f6bb9b7397088e800561b7d270fd120cb2
Safa Falaqi <sfalaqi@gmail.com>
2021-12-14 18:29:52 +0300

    Initial Commit

---
+++ Local Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Local Notifications
+//
+//  Created by Safa Falaqi on 14/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
